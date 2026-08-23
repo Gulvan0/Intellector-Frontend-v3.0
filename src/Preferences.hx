8a13e1647ffb0f9cc09e0434a6b3f9ddb4a40ee8
Gulvan <mitmitmitmit@yandex.ru>
2026-08-23 21:01:08 +0200

    Add general preferences: board coordinates, premoves, branching mode, etc.
    Replace the placeholder darkmode preference with Intellector's actual
    general-tab preferences (board coordinates, premoves, branching tab
    type, turn color indicators, silent challenges, follow latest move),
    with English/Russian locale strings, and widen the preference modal
    to fit the longer option labels.

--- src/Preferences.hx
+++ src/Preferences.hx
@@ -3,5 +3,10 @@ import haxefolio.preferences.PreferenceRegistry;
 class Preferences extends PreferenceRegistry
 {
     public static final language = PreferenceRegistry.locale("general", "language");
-    public static final darkMode = PreferenceRegistry.toggle("general", "darkmode", false);
+    public static final boardCoordinates = PreferenceRegistry.option("general", "board_coordinates", ["all", "files_only", "none"], "all");
+    public static final premoveEnabled = PreferenceRegistry.toggle("general", "premoves", false);
+    public static final branchingTabType = PreferenceRegistry.option("general", "branching_type", ["tree", "outline", "plain_text"], "tree");
+    public static final branchingTurnColorIndicators = PreferenceRegistry.toggle("general", "branching_turn_color_indicators", true);
+    public static final silentChallenges = PreferenceRegistry.toggle("general", "silent_challenges", false);
+    public static final followLatestMove = PreferenceRegistry.option("general", "follow_latest_move", ["always", "own_game_only", "never"], "own_game_only");
 }
